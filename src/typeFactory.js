539ca2c3d80b0c064af401dbdcd12342bd45ab84
Dave Moore <themoonrat@gmail.com>
2017-07-30 16:29:09 +0100

    Playing with potentially new way of making this work

---
+++ src/typeFactory.js
@@ -0,0 +1,51 @@
+import isArray from 'lodash-es/isArray';
+import isFunction from 'lodash-es/isFunction';
+import isObject from 'lodash-es/isObject';
+import isString from 'lodash-es/isString';
+
+const typeMap = {};
+
+export default {
+    addType(typeName, toJson, fromJson) {
+        if (!isString(typeName) || typeMap[typeName] || !isFunction(toJson) || !isFunction(fromJson)) {
+            return;
+        }
+
+        typeMap[typeName] = {
+            toJson,
+            fromJson,
+        };
+    },
+
+    toJson(instance, propertyNames) {
+        if (!isObject(instance) || !isArray(propertyNames)) {
+            return;
+        }
+
+        const json = {};
+
+        for (let i = 0; i < propertyNames.length; ++i) {
+            const propertyName = propertyNames[i];
+
+            if (typeMap[propertyName]) {
+                const propertyToJson = typeMap[propertyName].toJson;
+
+                json[propertyName] = propertyToJson(instance, propertyName);
+            }
+        }
+    },
+
+    fromJson(instance, json) {
+        if (!isObject(instance) || !isObject(json)) {
+            return;
+        }
+
+        for (const [key, value] of Object.entries(json)) {
+            if (typeMap[key]) {
+                const propertyFromJson = typeMap[key].fromJson;
+
+                instance[key] = propertyFromJson(instance, value);
+            }
+        }
+    },
+};
--- src/types/DisplayObject.js
+++ src/types/DisplayObject.js
@@ -2,98 +2,39 @@ import isObject from 'lodash-es/isObject';
 import isBoolean from 'lodash-es/isBoolean';
 import isNumber from 'lodash-es/isNumber';
 
-PIXI.DisplayObject.prototype.toJSON = function toJSON() {
+import typeFactory from '../typeFactory';
+import propertyFactory from '../propertyFactory';
+
+const properties = [
+    { name: 'alpha', type: 'number' },
+    { name: 'buttonMode', type: 'boolean' },
+    { name: 'height', type: 'number' },
+    { name: 'interactive', type: 'boolean' },
+    { name: 'interactiveChildren', type: 'boolean' },
+    { name: 'pivot', type: 'point' },
+    { name: 'rotation', type: 'number' },
+    { name: 'scale', type: 'point' },
+    { name: 'visible', type: 'boolean' },
+    { name: 'width', type: 'number' },
+    { name: 'x', type: 'number' },
+    { name: 'y', type: 'number' },
+];
+
+function toJson(instance) {
     return {
-        alpha: this.alpha,
-        buttonMode: this.buttonMode,
-        height: this.height,
-        interactive: this.interactive,
-        interactiveChildren: this.interactiveChildren,
-        pivot: {
-            x: this.pivot.x,
-            y: this.pivot.y,
-        },
-        rotation: this.rotation,
-        scale: {
-            x: this.scale.x,
-            y: this.scale.y,
-        },
-        visible: this.visible,
-        width: this.width,
-        x: this.x,
-        y: this.y,
-
+        type: 'DisplayObject',
+        properties: propertyFactory.toJson(instance, properties),
     };
-};
+}
 
-PIXI.DisplayObject.prototype.updateViaJSON = function updateViaJSON(json) {
-    if (isNumber(json.alpha)) {
-        this.alpha = json.alpha;
+function fromJson(json, instance) {
+    if (!instance) {
+        instance = new PIXI.DisplayObject();
     }
 
-    if (isBoolean(json.buttonMode)) {
-        this.buttonMode = json.buttonMode;
-    }
+    propertyFactory.fromJson(json.properties);
 
-    if (isNumber(json.height)) {
-        this.height = json.height;
-    }
+    return instance;
+}
 
-    if (isBoolean(json.interactive)) {
-        this.interactive = json.interactive;
-    }
-
-    if (isBoolean(json.interactiveChildren)) {
-        this.interactiveChildren = json.interactiveChildren;
-    }
-
-    if (isObject(json.pivot)) {
-        if (isNumber(json.pivot.x)) {
-            this.pivot.x = json.pivot.x;
-        }
-
-        if (isNumber(json.pivot.y)) {
-            this.pivot.y = json.pivot.y;
-        }
-    }
-
-    if (isNumber(json.rotation)) {
-        this.rotation = json.rotation;
-    }
-
-    if (isObject(json.scale)) {
-        if (isNumber(json.scale.x)) {
-            this.scale.x = json.scale.x;
-        }
-        if (isNumber(json.scale.y)) {
-            this.scale.y = json.scale.y;
-        }
-    }
-
-    if (isBoolean(json.visible)) {
-        this.visible = json.visible;
-    }
-
-    if (isNumber(json.width)) {
-        this.width = json.width;
-    }
-
-    if (isNumber(json.x)) {
-        this.x = json.x;
-    }
-    if (isNumber(json.y)) {
-        this.y = json.y;
-    }
-};
-
-PIXI.DisplayObject.fromJSON = function fromJSON(json, parent) {
-    const displayObject = new PIXI.DisplayObject();
-
-    displayObject.updateViaJSON(json);
-
-    if (parent && parent.addChild) {
-        parent.addChild(displayObject);
-    }
-
-    return displayObject;
-};
+typeFactory.addType('DisplayObject', toJson, fromJson);
